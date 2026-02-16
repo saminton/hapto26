e80b0f6fe448d396cf8349adcdfb63bce6b73af6
saminton <sam.minton@live.com>
2026-02-16 11:04:11 +0100

    plugin updates

--- www/wp-content/plugins/acf-autosize/src/js/index.js
+++
@@ -1,7 +0,0 @@
-if (typeof acf !== 'undefined') {
-	require('./textarea')
-	require('./wysiwyg')
-	if (typeof ACFAutosize !== 'undefined' && ACFAutosize.enabledByDefault) {
-		window.jQuery('body').addClass('acf-autosize-enabled')
-	}
-}
